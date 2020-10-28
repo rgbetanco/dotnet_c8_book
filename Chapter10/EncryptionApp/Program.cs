d226173ba9454cf647d664550726c71ebf6d2365
rgbetanco <rgbetanco@hotmail.com>
2020-10-28 15:26:54 +0800

    chapter 10

---
+++ Chapter10/EncryptionApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+using CryptographyLib;
+
+namespace EncryptionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var plainText = "Hello World";
+            var password = "pliocene";
+            var resE = Protector.Encrypt(plainText, password);
+            WriteLine(resE);
+            var resD = Protector.Decrypt(resE, password);
+            WriteLine(resD);
+        }
+    }
+}
---
+++ Chapter10/HashingApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+using CryptographyLib;
+
+namespace HashingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Regisering Alice with Pa$$word");
+            var alice = Protector.Register("Alice","Pas$$wOrd");
+            WriteLine($"Name: {alice.Name}");
+            WriteLine($"Salt: {alice.Salt}");
+            WriteLine("Password(salted and hashed):{0}", arg0: alice.SaltedHashedPassword);
+            WriteLine();
+            Write("Enter a new user to register: ");
+            string username = ReadLine();
+            Write($"Enter a password for {username}:");
+            string password = ReadLine();
+            var user = Protector.Register(username, password);
+            WriteLine($"Name: {user.Name}");
+            WriteLine("Password (salted and hashed): {0}", arg0: user.SaltedHashedPassword);
+            WriteLine();
+            bool correctPassword = false;
+            while(!correctPassword){
+                Write("Enter a username to log in:");
+                string loginUsername = ReadLine();
+                Write("Enter a password to log in:");
+                string loginPassword = ReadLine();
+                correctPassword = Protector.CheckPassword(loginUsername, loginPassword);
+                if(correctPassword){
+                    WriteLine($"Correct!{loginUsername} has been logged in.");
+                } else {
+                    WriteLine("Invalid username or password. Try again.");
+                }
+            }
+        }
+    }
+}
