71f9ceffbd2b47b8f055832654d9849826e8fa49
Ronald <rgbetanco@gmail.com>
2020-11-15 18:37:39 +0800

    ILogger for database

--- Chapter11/WorkWithEFCore/ConsoleLogger.cs
+++ Chapter11/WorkWithEFCore/ConsoleLogger.cs
@@ -1,16 +1,48 @@
+using System;
+using Microsoft.Extensions.Logging;
+using static System.Console;
 namespace WorkWithEFCore
 {
-    public class ConsoleLoggerProvider: ILoggerProvider 
+    public class ConsoleLoggerProvider : ILoggerProvider 
     {
         public ILogger CreateLogger(string categoryName){
             return new ConsoleLogger();
         }
-        public void Dispose(){}
+        public void Dispose(){ }
     }
-    public class ConsoleLogger: ILogger
+    public class ConsoleLogger : ILogger
     {
         public IDisposable BeginScope<TState>(TState state){
             return null;
+        }
+        public bool IsEnabled(LogLevel logLevel){
+            switch (logLevel)
+            {
+                case LogLevel.Trace:
+                case LogLevel.Information:
+                case LogLevel.None:
+                    return false;
+                case LogLevel.Debug:
+                case LogLevel.Warning:
+                case LogLevel.Error:
+                case LogLevel.Critical:
+                default:
+                    return true;
+            };
+        }
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter){
+            //if (eventId.Id == 20100){
+                //log the level and the event identifier
+                Write($"Level: {logLevel}, Event ID: {eventId}");
+                //Only output the state or the exception if they exist
+                if(state != null){
+                    Write($"State: {state}");
+                }
+                if(exception != null){
+                    Write($"Exception: {exception.Message}");
+                }
+                WriteLine();
+            //}
         }
     }
 }
--- Chapter11/WorkWithEFCore/Program.cs
+++ Chapter11/WorkWithEFCore/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using static System.Console;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System.Linq;
 
 namespace WorkWithEFCore
@@ -9,14 +12,18 @@ namespace WorkWithEFCore
     {
         static void Main(string[] args)
         {
-            QueryingProducts();
-            //QueryingCategories();
+            //QueryingProducts();
+            QueryingCategories();
         }
 
         static void QueryingCategories(){
             using (var db = new Northwind()){
+                var loggerFactory = db.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(new ConsoleLoggerProvider());
                 WriteLine("Categories and how many products they have:");
-                IQueryable<Category> cats = db.Categories.Include(c=>c.Products);
+                IQueryable<Category> cats = db.Categories
+                .Include(c=>c.Products)
+                .TagWith("Product filtered by price");
                 foreach(Category cat in cats){
                     WriteLine($"{cat.CategoryName} has {cat.Products.Count} products");
                 }
@@ -25,6 +32,8 @@ namespace WorkWithEFCore
 
         static void QueryingProducts(){
             using (var db = new Northwind()){
+                var loggerFactory = db.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(new ConsoleLoggerProvider());
                 WriteLine("Products that cost more than a price, highest at top");
                 string input;
                 decimal price;
@@ -32,7 +41,9 @@ namespace WorkWithEFCore
                     Write("Enter a product price"); 
                     input = ReadLine();
                 } while(!decimal.TryParse(input, out price));
-                IOrderedEnumerable<Product> prods = db.Products.AsEnumerable().Where(product => product.Cost > price).OrderByDescending(product => product.Cost);
+                IOrderedEnumerable<Product> prods = db.Products
+                .AsEnumerable().Where(product => product.Cost > price)
+                .OrderByDescending(product => product.Cost);
                 foreach(Product item in prods){
                     WriteLine(
                         "{0}:{1} costs {2:$#,##0.00} and has {3} in stock.",
