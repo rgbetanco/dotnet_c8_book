8d775c0811a1e2082fc4d7a02542df0b712a58c9
rgbetanco <rgbetanco@hotmail.com>
2020-11-16 16:59:37 +0800

    before simplifying the code in LinqWithArrayOfStrings

---
+++ Chapter12/LinqWithObjects/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+using System.Linq;
+
+namespace LinqWithObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinqWithArrayOfStrings();
+        }
+
+        static void LinqWithArrayOfStrings(){
+            var names = new string[]{"Michael","Pam","Jim","Dwight","Angela","Kevin","Toby","Creed"};
+            var query = names.Where(new Func<string, bool>(NameLongerThanFour));
+            foreach (string item in query)
+            {
+                WriteLine(item);
+            }
+        }
+        static bool NameLongerThanFour(string name){
+            return name.Length > 4;
+        } 
+    }
+}
