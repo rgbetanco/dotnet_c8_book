3371990b1746bd0db59a54ed8008a4b462cae638
rgbetanco <rgbetanco@hotmail.com>
2020-11-18 10:01:19 +0800

    add gitignore

--- Chapter12/LinqWithObjects/Program.cs
+++ Chapter12/LinqWithObjects/Program.cs
@@ -8,14 +8,18 @@ namespace LinqWithObjects
     {
         static void Main(string[] args)
         {
-            LinqWithArrayOfStrings();
+            //LinqWithArrayOfStrings();
+            LinqWithArrayOfExceptions();
         }
 
         static void LinqWithArrayOfStrings(){
             var names = new string[]{"Michael","Pam","Jim","Dwight","Angela","Kevin","Toby","Creed"};
             //var query = names.Where(new Func<string, bool>(NameLongerThanFour));
             //var query = names.Where(NameLongerThanFour);
-            var query = names.Where(name => name.Length > 4);
+            var query = names
+                .Where(name => name.Length > 4)
+                .OrderBy(name => name.Length)
+                .ThenBy(name => name);
             foreach (string item in query)
             {
                 WriteLine(item);
@@ -23,6 +27,24 @@ namespace LinqWithObjects
         }
         static bool NameLongerThanFour(string name){
             return name.Length > 4;
-        } 
+        }
+        static void LinqWithArrayOfExceptions(){
+            var errors = new Exception[]{
+                new ArgumentException(),
+                new SystemException(),
+                new IndexOutOfRangeException(),
+                new InvalidOperationException(),
+                new NullReferenceException(),
+                new InvalidCastException(),
+                new OverflowException(),
+                new DivideByZeroException(),
+                new ApplicationException()
+            };
+            var numberErrors = errors.OfType<ArithmeticException>();
+            foreach (var error in numberErrors)
+            {
+                WriteLine(error);
+            }
+        }
     }
 }
